60cf6a70c5d2265f57b5e7a58656fab7955d030f
Nathan Berger <nberger101@gmail.com>
2023-08-24 14:54:15 -0400

    set up 2 routes

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get("/")
+def show_homepage():
+    return render_template("survey_start.html")
+
+@app.post("/begin")
+def show_question():
+
+    question = survey.questions[0]
+
+    return render_template("question.html", question=question)
